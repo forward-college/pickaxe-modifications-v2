7b3045f63618c875d7901f95ad89d50722fed56d
leo-reche <ladreche@gmail.com>
2026-03-12 16:46:58 +0000

    .

--- staff/files/main.js
+++
@@ -1,8 +0,0 @@
-console.log("my name is julia")
-
-// const span = document.querySelector(
-//     'span.text-\\[20px\\].max-\\[1024px\\]\\:text-\\[20px\\].max-\\[899px\\]\\:text-\\[18px\\].min-w-0.truncate.whitespace-nowrap.text-left.font-semibold.transition-all.duration-300.ease-in-out.max-\\[859px\\]\\:max-w-full.ml-1.mr-3.max-w-\\[200px\\].opacity-100'
-//     );
-//     if (span) {
-//     span.textContent = "Menu";
-// }
